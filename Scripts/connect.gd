6e938cf337c23b64d251d81030567b567e884752
rat-balls <delalande.ethan@gmail.com>
2025-04-11 11:19:21 +0200

    Added new MQTT publishes

--- Scripts/connect.gd
+++ Scripts/connect.gd
@@ -3,7 +3,8 @@ extends Control
 @onready var mqtt:  = $MQTT
 
 signal beerLevel(amount)
-signal pause()
+signal lift()
+signal putback()
 
 func _ready():
 	mqtt.set_user_pass("pluton3", "nabil")
@@ -15,8 +16,11 @@ func _on_mqtt_received_message(topic, message):
 		beerLevel.emit(message.to_float())
 	elif(topic == "jeuboire/pump"):
 		if(message == "lift"):
-			pause.emit()
+			lift.emit()
+		if(message == "putback"):
+			putback.emit()
 
 func _on_mqtt_broker_connected():
 	mqtt.subscribe("jeuboire/beerLvl")
 	mqtt.subscribe("jeuboire/pump")
+	mqtt.publish("jeuboire/pump", "unpause")
--- Scripts/pause.gd
+++ Scripts/pause.gd
@@ -5,6 +5,8 @@ var PauseScreen: ColorRect;
 @onready var mqtt = $"../../MQTT"
 var menu: PackedScene; 
 
+enum PAUSED {TOGGLE, PAUSE, UNPAUSE}
+
 func _ready():
 	menu = load("res://Scenes/menu.tscn")
 	PauseScreen = get_parent().get_parent().get_node("HUD/UI/PauseScreen");
@@ -13,16 +15,25 @@ func _process(delta):
 	if(get_tree().paused == true && Input.is_action_just_pressed("Restart")):
 		Restart()
 
-func PauseGame():
-	var paused = !get_tree().paused;
-	get_tree().paused = paused;
-	PauseScreen.visible = paused;
-	mqtt.publish("jeuboire/pump", "pause")
+func PauseGame(enP: PAUSED = PAUSED.TOGGLE):
+	if(enP == PAUSED.TOGGLE):
+		var paused = !get_tree().paused;
+		get_tree().paused = paused;
+		PauseScreen.visible = paused;
+		mqtt.publish("jeuboire/pump", "pause")
+	elif(enP == PAUSED.PAUSE):
+		get_tree().paused = true;
+		PauseScreen.visible = true;
+		mqtt.publish("jeuboire/pump", "pause")
+	elif(enP == PAUSED.UNPAUSE):
+		get_tree().paused = false;
+		PauseScreen.visible = false;
+		mqtt.publish("jeuboire/pump", "unpause")
 
 
 func _on_death_body_entered(body: Node2D):
 	if(body.get_groups()[0].contains("Player")):
-		PauseGame();
+		PauseGame(1);
 		PauseScreen.get_node("DrinkingProgress").text = "YOU DROWNED.";
 		PauseScreen.get_node("Restart").visible = true;
 		PauseScreen.get_node("Menu").visible = true;
@@ -34,11 +45,14 @@ func _on_restart_button_up():
 
 func Restart():
 	get_tree().call_deferred("reload_current_scene");
-	PauseGame();
+	PauseGame(2);
 
 func _on_menu_button_up():
-	PauseGame();
+	PauseGame(2);
 	get_tree().change_scene_to_packed(menu)
 
-func _on_control_pause():
-	PauseGame()
+func _on_control_lift():
+	PauseGame(1)
+
+func _on_control_putback():
+	PauseGame(2)
